ccdcb2af87fdbf19d86c94b88b91099dba662081
Jeff Scott <jpscott@ranken.edu>
2022-08-21 17:38:24 -0500

    Initial Commit

---
+++ Homework/Exercises/Extra Starts/Chapter 10/InvoiceTotal/InvoiceTotal/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InvoiceTotal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+        }
+
+        public bool IsValidData()
+        {
+        }
+
+        public bool IsPresent(TextBox textBox, string name)
+        {
+        }
+
+        public bool IsDecimal(TextBox textBox, string name)
+        {
+        }
+
+        public bool IsWithinRange(TextBox textBox, string name,
+            decimal min, decimal max)
+        {
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
