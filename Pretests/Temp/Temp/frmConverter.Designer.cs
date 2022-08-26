cab9bb3ab0c6097152e4f009e2fdf97e4df62d04
Jeff Scott <jpscott@ranken.edu>
2022-08-26 10:47:05 -0500

    HOT1 Test

---
+++ Pretests/Temp/Temp/frmConverter.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Temp
+{
+    partial class frmConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblMiles = new System.Windows.Forms.Label();
+            this.txtMiles = new System.Windows.Forms.TextBox();
+            this.txtKilometers = new System.Windows.Forms.TextBox();
+            this.lblKilometers = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(67, 346);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(144, 62);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblMiles
+            // 
+            this.lblMiles.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiles.Location = new System.Drawing.Point(93, 81);
+            this.lblMiles.Name = "lblMiles";
+            this.lblMiles.Size = new System.Drawing.Size(166, 38);
+            this.lblMiles.TabIndex = 2;
+            this.lblMiles.Text = "Miles:";
+            this.lblMiles.Click += new System.EventHandler(this.lblMiles_Click);
+            // 
+            // txtMiles
+            // 
+            this.txtMiles.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMiles.Location = new System.Drawing.Point(319, 81);
+            this.txtMiles.Name = "txtMiles";
+            this.txtMiles.Size = new System.Drawing.Size(248, 38);
+            this.txtMiles.TabIndex = 3;
+            this.txtMiles.TextChanged += new System.EventHandler(this.txtMiles_TextChanged);
+            // 
+            // txtKilometers
+            // 
+            this.txtKilometers.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKilometers.Location = new System.Drawing.Point(319, 186);
+            this.txtKilometers.Name = "txtKilometers";
+            this.txtKilometers.Size = new System.Drawing.Size(248, 38);
+            this.txtKilometers.TabIndex = 5;
+            // 
+            // lblKilometers
+            // 
+            this.lblKilometers.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKilometers.Location = new System.Drawing.Point(93, 186);
+            this.lblKilometers.Name = "lblKilometers";
+            this.lblKilometers.Size = new System.Drawing.Size(142, 38);
+            this.lblKilometers.TabIndex = 4;
+            this.lblKilometers.Text = "Kilometers:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(274, 346);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(144, 62);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(496, 346);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(144, 62);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtKilometers);
+            this.Controls.Add(this.lblKilometers);
+            this.Controls.Add(this.txtMiles);
+            this.Controls.Add(this.lblMiles);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "frmConverter";
+            this.Text = "MIles to Kilometers Convert";
+            this.Load += new System.EventHandler(this.frmConverter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblMiles;
+        private System.Windows.Forms.TextBox txtMiles;
+        private System.Windows.Forms.TextBox txtKilometers;
+        private System.Windows.Forms.Label lblKilometers;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Pretests/Temp/Temp/frmConverter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temp
+{
+    public partial class frmConverter : Form
+    {
+        public frmConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            int x = 7;
+            int y = 5;
+            int z = x / y;
+            MessageBox.Show(z.ToString());
+        }
+
+        private void txtMiles_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblMiles_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmConverter_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi From Calculate Button",
+                            "Calculate Button Enter Event",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+    }
+}
